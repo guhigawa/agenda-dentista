5313591cb80f1247b31cea12dae2e348662a2fe1
guhigawa <guhigawa@gmail.com>
2026-06-16 02:46:18 +0100

    add calendar module para implementar o modulo calendar

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { FirebaseModule } from './firebase/firebase.module';
 import { PacientesModule } from './pacientes/pacientes.module';
+import { CalendarModule } from './calendar/calendar.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { PacientesModule } from './pacientes/pacientes.module';
     }),
     FirebaseModule,
     PacientesModule,
+    CalendarModule,
   ],
 })
 export class AppModule {}
